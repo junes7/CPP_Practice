76c9c2655d4a9f8730890b23726c14952e3a9566
2junes <2junes@naver.com>
2024-10-15 22:36:50 +0900

    solve namespace problem

---
+++ 1. function and I_O/1-5.introduction_to_namespace/NameSp1.cpp
@@ -0,0 +1,23 @@
+/*
+    BestCom이 정의한 함수
+    ProgCom이 정의한 함수
+*/
+#include <iostream>
+
+namespace BestComImpl {
+void SimpleFunc(void) {
+    std::cout << "BestCom이 정의한 함수" << std::endl;
+}
+}  // namespace BestComImpl
+
+namespace ProgComImpl {
+void SimpleFunc(void) {
+    std::cout << "ProgCom이 정의한 함수" << std::endl;
+}
+}  // namespace ProgComImpl
+
+int main(void) {
+    BestComImpl::SimpleFunc();
+    ProgComImpl::SimpleFunc();
+    return 0;
+}
