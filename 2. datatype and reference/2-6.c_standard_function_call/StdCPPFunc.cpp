4dacfd0f7a3aa383562b5422e9bb78406c5be077
2junes <2junes@naver.com>
2024-10-17 22:57:57 +0900

    study how to call C stadard function in C++

---
+++ 2. datatype and reference/2-6.c_standard_function_call/StdCPPFunc.cpp
@@ -0,0 +1,18 @@
+/*
+    sin(0.14)
+    abs(-1.25)
+    Result 0.139543
+    Result 1.250000
+*/
+#include <math.h>
+
+#include <cstdio>
+#include <cstring>
+
+int main(void) {
+    char str1[] = "Result";
+    char str2[30];
+    strcpy(str2, str1);
+    printf("%s %f\n", str1, sin(0.14));
+    printf("%s %f\n", str2, abs(-1.25));
+}
