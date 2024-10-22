10e6b165a7a5a932a50d11e209134eb7e80dccc6
2junes <2junes@naver.com>
2024-10-22 23:55:34 +0900

    study encapsulation with medicine class

---
+++ 4. class completion/4-2.encapsulation/Encaps1.cpp
@@ -0,0 +1,46 @@
+/*
+    콧물이 싹~ 납니다.
+    재채기가 멎습니다.
+    코가 뻥 뚫립니다.
+*/
+#include <iostream>
+using namespace std;
+
+class SinivelCap {
+   public:
+    void Take() const {
+        cout << "콧물이 싹~ 납니다." << endl;
+    }
+};
+
+class SneezeCap {
+   public:
+    void Take() const {
+        cout << "재채기가 멎습니다." << endl;
+    }
+};
+
+class SnuffleCap {
+   public:
+    void Take() const {
+        cout << "코가 뻥 뚫립니다." << endl;
+    }
+};
+
+class ColdPatient {
+   public:
+    void TakeSinivelCap(const SinivelCap &cap) const { cap.Take(); }
+    void TakeSneezeCap(const SneezeCap &cap) const { cap.Take(); }
+    void TakeSnuffleCap(const SnuffleCap &cap) const { cap.Take(); }
+};
+
+int main(void) {
+    SinivelCap scap;
+    SneezeCap zcap;
+    SnuffleCap fcap;
+    ColdPatient cp;
+    cp.TakeSinivelCap(scap);
+    cp.TakeSneezeCap(zcap);
+    cp.TakeSnuffleCap(fcap);
+    return 0;
+}
