05c54b6c8004888123efef6b7ca11b79e89fce81
2junes <2junes@naver.com>
2024-11-12 23:34:11 +0900

    modified spaces in class

--- 9.principle_of_virtual_and_multiple_inheritance/9-1.member_function_and_operating_principle_of_virtual_function/VirtualInternal.cpp
+++ 9.principle_of_virtual_and_multiple_inheritance/9-1.member_function_and_operating_principle_of_virtual_function/VirtualInternal.cpp
@@ -6,12 +6,9 @@ class AAA {
     int num1;
 
    public:
-    virtual void Func1() {
-        cout << "Func1" << endl;
-    }
-    virtual void Func2() {
-        cout << "Func2" << endl;
-    }
+    virtual void Func1() const { cout << "Func1" << endl; }
+
+    virtual void Func2() const { cout << "Func2" << endl; }
 };
 
 class BBB : public AAA {
@@ -19,19 +16,15 @@ class BBB : public AAA {
     int num2;
 
    public:
-    virtual void Func1() {
-        cout << "BBB::Func1" << endl;
-    }
-    void Func3() {
-        cout << "Func3" << endl;
-    }
+    virtual void Func1() const { cout << "BBB::Func1" << endl; }
+
+    void Func3() const { cout << "Func3" << endl; }
 };
 
 int main(void) {
     AAA *aptr = new AAA();
-    aptr->Func1();
-
     BBB *bptr = new BBB();
+    aptr->Func1();
     bptr->Func1();
     return 0;
 }
